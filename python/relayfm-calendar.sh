609f01911b6910eb09b561173f2e80f4a900af72
Rich Somerfield <rs@richsomerfield.com>
2015-12-08 21:58:37 +0000

    Comment tweak

--- python/relayfm-calendar.sh
+++ python/relayfm-calendar.sh
@@ -4,7 +4,7 @@ CURRENT_TIMEZONE=`date +%Z`
 
 CALENDAR_URL_HTML='https://www.google.com/calendar/htmlembed?showTitle=0&showPrint=0&showTabs=0&showCalendars=0&mode=AGENDA&height=600&wkst=1&bgcolor=%23FFFFFF&src=relay.fm_t9pnsv6j91a3ra7o8l13cb9q3o@group.calendar.google.com&color=%23182C57&ctz='$CURRENT_TIMEZONE
 
-# get the whole HTML, and reformat it with `tidy` and save it to a local file we can parse
+# get the whole HTML, and reformat it with `tidy` and process it with python script
 curl -sfL "$CALENDAR_URL_HTML" \
 | tidy \
     --char-encoding utf8 \
